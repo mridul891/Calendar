0cb9169d3cd52bcba50d2231e174f6dc26d1ae4e
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-10-13 22:21:25 +0530

    Creation of constant data and a new div

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import Calendar from "./Components/Calendar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
+  const [fullscreen, setFullscreen] = useState(false);
+
+  const handleClick = () => {
+    setFullscreen((fullscreen) => !fullscreen);
+  };
   return (
-    <div className="flex justify-center items-center  h-screen bg-black text-white">
-      <Calendar />
+    <div className="flex justify-around items-center w-screen h-screen bg-black text-white">
+      <div className=" h-full w-full flex items-center justify-center">
+        <Calendar />
+      </div>
+      <div className={` h-full ${fullscreen ? "w-[40vw]" : "none"}`}>
+        <div
+          className={` h-full ${fullscreen ? "bg-gray-400" : "bg-black"} `}
+          onClick={handleClick}
+        >
+          <FontAwesomeIcon
+            icon="fa-solid fa-window-maximize"
+            style={{ color: "#ffffff" }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
